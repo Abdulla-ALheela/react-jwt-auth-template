c7008b0958cb8399332ffbaf8eb9ad3581c77c42
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-02-10 20:49:18 +0300

    Contexts

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,26 @@
+import { useContext } from 'react';
 import { Link } from 'react-router';
 
+import { UserContext } from '../../contexts/UserContext';
+
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li><Link to='/sign-up'>Sign Up</Link></li>
-      </ul>
-    </nav>
-  );
+
+    const { user } = useContext(UserContext);
+
+    return (
+        <nav>
+            {user ? (
+                <ul>
+                    <li>Welcome, {user.username}</li>
+                </ul>
+            ) : (
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/sign-up'>Sign Up</Link></li>
+                </ul>
+            )}
+        </nav>
+    );
 };
 
-export default NavBar;
+export default NavBar
