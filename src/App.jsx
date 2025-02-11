93cff0efb5320055e8611a0473ff8afce19e3585
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-02-11 20:01:15 +0300

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
 import { Routes, Route } from 'react-router'; 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import Landing from './components/Landing/Landing';
-
+import SignInForm from './components/SignInForm/SignInForm';
+import Dashboard from './components/Dashboard/Dashboard';
+import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar />
       <Routes>
-      <Route path='/' element={<Landing />} />
+      <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
         <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
       </Routes>
     </>
   );
